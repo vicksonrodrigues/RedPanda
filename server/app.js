f5c641f6835b4d188c1c292f2a9a830369615038
Vickson Rodrigues <55660155+vicksonrodrigues@users.noreply.github.com>
2023-07-31 16:03:36 +0530

    Change Database connection sequence

--- server/app.js
+++ server/app.js
@@ -1,15 +1,14 @@
 require('express-async-errors');
 const express = require('express');
-const mongoose = require('mongoose');
+
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 // configs
 const corsOptions = require('./config/corsOptions');
-const config = require('./config/configVar');
+
 // middleware
 const morganMiddleware = require('./middlewares/morganMiddleware');
 const { unknownEndpoint, errorHandler } = require('./middlewares/errorHandler');
-const logger = require('./middlewares/logger');
 
 // controllers
 const reviewRouter = require('./controllers/reviews');
@@ -24,19 +23,6 @@ const employeeRouter = require('./controllers/employees');
 const verifyJWT = require('./middlewares/verifyJWT');
 
 const app = express();
-mongoose.set('strictQuery', false);
-if (process.env.NODE_ENV === 'development') {
-  logger.info(`connecting to ${config.MONGODB_URI}`);
-}
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB');
-    console.log('connected to MongoDB');
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message);
-  });
 
 app.use(morganMiddleware);
 app.use(cors(corsOptions));
--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/configVar');
 const logger = require('./middlewares/logger');
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
-  console.log(`Server running on port ${config.PORT}`);
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    if (process.env.NODE_ENV === 'development') {
+      logger.info(`connecting to ${config.MONGODB_URI}`);
+    }
+    const conn = await mongoose.connect(config.MONGODB_URI);
+    logger.info(`MongoDB Connected: ${conn.connection.host}`);
+    console.log('connected to MongoDB');
+  } catch (error) {
+    logger.error('error connecting to MongoDB:', error.message);
+  }
+};
+connectDB().then(() => {
+  app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`);
+    console.log(`Server running on port ${config.PORT}`);
+  });
 });
